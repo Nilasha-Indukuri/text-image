8b2e85f27e036040fda2c8cf7ffb2abb29198142
Sai Nilasha Varma Indukuri <145094325+Nilasha-Indukuri@users.noreply.github.com>
2025-11-09 21:27:59 -0500

    Text-to-Image

--- model.py
+++ model.py
@@ -1,25 +1,24 @@
 import torch
 import torch.nn as nn
 
-# Stage 1 Generator - 64x64 image generation
 class GeneratorStage1(nn.Module):
     def __init__(self, noise_dim=100, text_dim=512, img_channels=3):
         super(GeneratorStage1, self).__init__()
         self.fc = nn.Linear(noise_dim + text_dim, 1024 * 4 * 4)
         self.net = nn.Sequential(
-            nn.ConvTranspose2d(1024, 512, 4, 2, 1), # 8x8
+            nn.ConvTranspose2d(1024, 512, 4, 2, 1),
             nn.BatchNorm2d(512),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(512, 256, 4, 2, 1), # 16x16
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(256, 128, 4, 2, 1), # 32x32
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(128, img_channels, 4, 2, 1), # 64x64
+            nn.ConvTranspose2d(128, img_channels, 4, 2, 1),
             nn.Tanh()
         )
     
@@ -30,23 +29,22 @@ class GeneratorStage1(nn.Module):
         return img
 
 
-# Stage 1 Discriminator - for 64x64 images
 class DiscriminatorStage1(nn.Module):
     def __init__(self, text_dim=512, img_channels=3):
         super(DiscriminatorStage1, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(img_channels, 64, 4, 2, 1), # 32x32
+            nn.Conv2d(img_channels, 64, 4, 2, 1),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(64, 128, 4, 2, 1), # 16x16
+            nn.Conv2d(64, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(128, 256, 4, 2, 1), # 8x8
+            nn.Conv2d(128, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(256, 512, 4, 2, 1), # 4x4
+            nn.Conv2d(256, 512, 4, 2, 1),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
         )
@@ -60,37 +58,36 @@ class DiscriminatorStage1(nn.Module):
         return out
 
 
-# Stage 2 Generator - 128x128 refinement
 class GeneratorStage2(nn.Module):
     def __init__(self, text_dim=512, img_channels=3):
         super(GeneratorStage2, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(img_channels, 64, 4, 2, 1), # 32x32
+            nn.Conv2d(img_channels, 64, 4, 2, 1),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(64, 128, 4, 2, 1), # 16x16
+            nn.Conv2d(64, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(128, 256, 4, 2, 1), #8x8
+            nn.Conv2d(128, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
         )
         self.fc = nn.Linear(256*8*8 + text_dim, 512*8*8)
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, 4, 2, 1), # 16x16
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(256, 128, 4, 2, 1), # 32x32
+            nn.ConvTranspose2d(256, 128, 4, 2, 1), 
             nn.BatchNorm2d(128),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(128, 64, 4, 2, 1), # 64x64
+            nn.ConvTranspose2d(128, 64, 4, 2, 1), 
             nn.BatchNorm2d(64),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(64, img_channels, 4, 2, 1), # 128x128
+            nn.ConvTranspose2d(64, img_channels, 4, 2, 1),
             nn.Tanh()
         )
     
@@ -104,27 +101,26 @@ class GeneratorStage2(nn.Module):
         return img128
 
 
-# Stage 2 Discriminator - for 128x128 images
 class DiscriminatorStage2(nn.Module):
     def __init__(self, text_dim=512, img_channels=3):
         super(DiscriminatorStage2, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(img_channels, 64, 4, 2, 1), # 64x64
+            nn.Conv2d(img_channels, 64, 4, 2, 1),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(64, 128, 4, 2, 1), # 32x32
+            nn.Conv2d(64, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(128, 256, 4, 2, 1), # 16x16
+            nn.Conv2d(128, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(256, 512, 4, 2, 1), # 8x8
+            nn.Conv2d(256, 512, 4, 2, 1),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(512, 1024, 4, 2, 1), # 4x4
+            nn.Conv2d(512, 1024, 4, 2, 1),
             nn.BatchNorm2d(1024),
             nn.LeakyReLU(0.2, inplace=True),
         )
